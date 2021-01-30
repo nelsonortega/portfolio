56ae99845cba12d45149288efc04e287b9276170
Nelson <neor9712@gmail.com>
2021-01-29 22:23:31 -0600

    Home and about

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
-const showMenu = (toggleId, navId) => {
-  const toggle = document.getElementById(toggleId)
-  const nav = document.getElementById(navId)
+const toggleNavigationMenu = () => {
+  const toggleButton = document.getElementById('nav-toggle')
+  const navigationMenu = document.getElementById('nav-menu')
 
-  if (toggle && nav) {
-    toggle.addEventListener('click', () => {
-      nav.classList.toggle('show')
+  if (toggleButton && navigationMenu) {
+    toggleButton.addEventListener('click', () => {
+      navigationMenu.classList.toggle('show')
     })
   }
 }
 
-showMenu('nav-toggle', 'nav-menu')
+toggleNavigationMenu()
+
+const navigationLinks = document.querySelectorAll('.nav-link')
+
+function navigate() {
+  navigationLinks.forEach(link => link.classList.remove('active'))
+  this.classList.add('active')
+
+  const navigationMenu = document.getElementById('nav-menu')
+  navigationMenu.classList.remove('show')
+}
+
+navigationLinks.forEach(link => link.addEventListener('click', navigate))
