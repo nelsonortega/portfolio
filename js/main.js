a8f591ba3cfb7ade8a1405cf0b12fbb18e33d10d
Nelson <neor9712@gmail.com>
2021-02-28 16:45:55 -0600

    Work and contact sections

--- js/main.js
+++ js/main.js
@@ -22,3 +22,28 @@ function navigate() {
 }
 
 navigationLinks.forEach(link => link.addEventListener('click', navigate))
+
+const scroll = ScrollReveal({
+  origin: 'top',
+  distance: '80px',
+  duration: 2000,
+  reset: true
+})
+
+scroll.reveal('.home-title', {})
+scroll.reveal('.button', {delay: 200})
+scroll.reveal('.home-image', {delay: 400})
+scroll.reveal('.home-social-icon', {interval: 200})
+
+scroll.reveal('.about-image', {})
+scroll.reveal('.about-subtitle', {delay: 200})
+scroll.reveal('.about-text', {delay: 400})
+
+scroll.reveal('.skills-subtitle', {}); 
+scroll.reveal('.skills-text', {}); 
+scroll.reveal('.skill-box', {interval: 200}); 
+scroll.reveal('.skills-image', {delay: 600});
+
+scroll.reveal('.work-image', {interval: 200}); 
+
+scroll.reveal('.contact-input', {interval: 200}); 
